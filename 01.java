b88e31edbd4418783ef7838ad65a5f58bb8261d5
jeyarajahananthia <anajeya26@gmail.com>
2025-05-17 10:21:53 +0000

    done

--- 01.java
+++ 01.java
@@ -1,4 +1,12 @@
 class Student{
+    String name;
+    int marks;
+
+    int getMarks(){
+        return marks;
+    }
+
+
     public static void main(String[] args){
         System.out.println("Hi");
     }
